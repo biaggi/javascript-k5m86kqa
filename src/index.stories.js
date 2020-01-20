d1c2fba0f7d5a057e91bd7522e6142448d738e1e
Jose Manuel <biaggi@gmail.com>
2020-01-20 09:12:02 +0000

    Intentando cambiar un atributo a ver si reacciona

--- src/index.stories.js
+++ src/index.stories.js
@@ -3,4 +3,15 @@ import { html } from "lit-html";
 
 export const story1 = () => html`
   <custom-element name="you"> </custom-element>
+
+  <button
+    @click=${() => {
+      console.log("test");
+      document
+        .getElementsByTagName("custom-element")[0]
+        .setAttribute("name", "me");
+    }}
+  >
+    test
+  </button>
 `;
